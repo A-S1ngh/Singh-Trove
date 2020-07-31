6e9a7e12931ea943c78d850359c151263fb803ea
Monte Cristo <asingh30@student.gsu.edu>
2020-07-31 14:52:27 -0400

    An introduction to Tuples in Python. Also added some more comments to my lists file. Also some readme thing.

--- Language Testing Files/Lists.py
+++ Language Testing Files/Lists.py
@@ -2,5 +2,5 @@ names = ["Abhi", "Ayush", "Vinai"] #This is a list. You can make changes to a li
 print(names[0])
 names.append("Indra") #Indra has now been put at slot 3
 print(names[3])
-names.sort()
-print(names)
+names.sort() #Sorts the names in Alphabetical Order. Lists have many more methods.
+print(f"Hello {names}")
---
+++ Language Testing Files/Tuples.py
@@ -0,0 +1,6 @@
+players = ("Kahwi", "Lebron", "Giannis") #This is a tuple. Order Matters, but you cannot make changes to it.
+#Tuples have only 2 methods, Index and Count.
+kcount = players.count("Kahwi")
+lindex = players.index("Lebron")
+print(f"The amount of times that Kahwi shows up is {kcount}")
+print(f"Lebron's index in this tuple is {lindex}")

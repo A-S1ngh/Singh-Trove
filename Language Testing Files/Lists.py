0e0c71d3442a44d91a2431ac1e29195ef1a8bafa
Monte Cristo <asingh30@student.gsu.edu>
2020-07-31 14:28:28 -0400

    Started with the basics of lists in Python.

---
+++ Language Testing Files/Lists.py
@@ -0,0 +1,6 @@
+names = ["Abhi", "Ayush", "Vinai"] #This is a list. You can make changes to a list and order matters.
+print(names[0])
+names.append("Indra") #Indra has now been put at slot 3
+print(names[3])
+names.sort()
+print(names)
